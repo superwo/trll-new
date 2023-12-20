db91f9483ac64b744d2d61c8fe333afa18237188
Radu <radu.avadanii@gmail.com>
2023-12-20 13:27:45 +0200

    feat: prisma.

--- app/(platform)/(dashboard)/organization/[organizationId]/page.jsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.jsx
@@ -1,5 +1,18 @@
 const OrganizationIdPage = () => {
-    return <div>Organization Page</div>;
+    return (
+        <div>
+            <form>
+                <input
+                    type="text"
+                    id="title"
+                    name="title"
+                    required
+                    placeholder="Enter a board title"
+                    className="border-black border p-1"
+                />
+            </form>
+        </div>
+    );
 };
 
 export default OrganizationIdPage;
