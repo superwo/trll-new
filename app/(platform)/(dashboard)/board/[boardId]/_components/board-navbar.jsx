ac0473153c2fac21e4fa861781ef717c6797a8a0
Radu <radu.avadanii@gmail.com>
2024-01-04 11:38:12 +0200

    feat: board delete action.

--- app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.jsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.jsx
@@ -1,13 +1,12 @@
 import { BoardTitleForm } from "./board-title-form";
-// import { BoardOptions } from "./board-options";
+import { BoardOptions } from "./board-options";
 
 export const BoardNavbar = async ({ data }) => {
     return (
         <div className="w-full h-14 z-[40] bg-black/50 fixed top-14 flex items-center px-6 gap-x-4 text-white">
             <BoardTitleForm data={data} />
             <div className="ml-auto">
-                Board Navbar
-                {/* <BoardOptions id={data.id} /> */}
+                <BoardOptions id={data.id} />
             </div>
         </div>
     );
